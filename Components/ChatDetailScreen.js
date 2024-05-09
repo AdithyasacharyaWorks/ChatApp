9de8b8ecbcabcd464ac0bf8c7a0340b339ae868e
AdithyasacharyaWorks <156585313+AdithyasacharyaWorks@users.noreply.github.com>
2024-05-09 12:58:13 +0530

    feature pdf upload and view
    feature pdf upload and view is been added

--- Components/ChatDetailScreen.js
+++ Components/ChatDetailScreen.js
@@ -6,13 +6,20 @@ import {
   TouchableOpacity,
   StyleSheet,
   ScrollView,
+  Linking,
+  Platform,
 } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
+import * as DocumentPicker from "expo-document-picker";
+import * as FileSystem from "expo-file-system";
+import * as IntentLauncher from "expo-intent-launcher";
 
 const ChatDetailScreen = ({ route }) => {
   const { chat } = route.params;
   const [message, setMessage] = useState("");
+  const [freezeMessage, setFreezeMessage] = useState(false);
   const [messages, setMessages] = useState(chat.conversation || []);
+  const [selectedFile, setSelectedFile] = useState(null);
   const scrollViewRef = useRef();
 
   useEffect(() => {
@@ -21,16 +28,63 @@ const ChatDetailScreen = ({ route }) => {
   }, [messages]);
 
   const handleMessageSend = () => {
-    if (message.trim() !== "") {
-      const newMessage = {
-        id: messages.length + 1,
-        sender: "You",
-        message,
-        isSender: true,
-        time: getCurrentTime(),
-      };
-      setMessages([...messages, newMessage]);
-      setMessage("");
+    if (freezeMessage) {
+      setFreezeMessage(false);
+    }
+    if (message.trim() !== "" || selectedFile) {
+      if (message.trim() !== "") {
+        const newMessage = {
+          id: messages.length + 1,
+          sender: "You",
+          message,
+          isSender: true,
+          time: getCurrentTime(),
+        };
+        setMessages([...messages, newMessage]);
+        setMessage("");
+        console.log(newMessage);
+      }
+      if (selectedFile) {
+        console.log("here inside selected file ");
+        const newMessage = {
+          id: messages.length + 1,
+          sender: "You",
+          message: `Document: ${selectedFile?.name}`,
+          fileURI: selectedFile?.uri, // Add file URI to message
+          isSender: true,
+          time: getCurrentTime(),
+        };
+
+        setMessages([...messages, newMessage]);
+        setSelectedFile(null);
+        console.log(newMessage);
+      }
+    }
+  };
+  const handleDocumentUpload = async () => {
+    console.log("here inside the handleDocument");
+    try {
+      const document = await DocumentPicker.getDocumentAsync({
+        type: "application/pdf", // Specify the type of document you want to allow
+      });
+
+      if (document.assets && document.assets.length > 0 && !document.canceled) {
+        // Move the document to the app's document directory
+        const asset = document.assets[0];
+        const newPath = `${FileSystem.documentDirectory}${asset.name}`;
+        await FileSystem.moveAsync({
+          from: asset.uri,
+          to: newPath,
+        });
+        setSelectedFile({ ...asset, uri: newPath });
+        console.log("Selected file:", asset);
+        setMessage(document?.assets[0]?.name);
+        setFreezeMessage(true);
+      } else {
+        console.warn("Invalid document object:", document);
+      }
+    } catch (error) {
+      console.error("Error picking document: ", error);
     }
   };
 
@@ -41,6 +95,34 @@ const ChatDetailScreen = ({ route }) => {
     return `${hours < 10 ? "0" + hours : hours}:${
       minutes < 10 ? "0" + minutes : minutes
     }`;
+  };
+
+  const handleFileClick = async (fileURI) => {
+    try {
+      let localUri;
+      if (Platform.OS === "android") {
+        const fileParts = fileURI.split("/");
+        const fileName = fileParts[fileParts.length - 1];
+        localUri = `${FileSystem.cacheDirectory}${fileName}`;
+        await FileSystem.copyAsync({ from: fileURI, to: localUri });
+      } else {
+        localUri = fileURI;
+      }
+      await FileSystem.getContentUriAsync(localUri)
+        .then((contentUri) => {
+          console.log("Content URI:", contentUri);
+          IntentLauncher.startActivityAsync("android.intent.action.VIEW", {
+            data: contentUri,
+            flags: 1, // FLAG_GRANT_READ_URI_PERMISSION
+            type: "application/pdf",
+          });
+        })
+        .catch((error) => {
+          console.error("Error opening file:", error);
+        });
+    } catch (error) {
+      console.error("Error handling file click:", error);
+    }
   };
 
   return (
@@ -62,7 +144,11 @@ const ChatDetailScreen = ({ route }) => {
             ]}
           >
             <View style={styles.messageContent}>
-              <View>
+              {msg.fileURI ? (
+                <TouchableOpacity onPress={() => handleFileClick(msg.fileURI)}>
+                  <Text style={styles.fileLink}>{msg.message}</Text>
+                </TouchableOpacity>
+              ) : (
                 <Text
                   style={
                     msg.isSender ? styles.senderMessage : styles.receiverMessage
@@ -70,20 +156,25 @@ const ChatDetailScreen = ({ route }) => {
                 >
                   {msg.message}
                 </Text>
-              </View>
-              <View>
-                <Text style={styles.timeText}>{msg.time}</Text>
-              </View>
+              )}
+              <Text style={styles.timeText}>{msg.time}</Text>
             </View>
           </View>
         ))}
       </ScrollView>
       <View style={styles.inputContainer}>
+        <TouchableOpacity
+          style={styles.attachmentButton}
+          onPress={handleDocumentUpload}
+        >
+          <MaterialIcons name="attach-file" size={24} color="#666" />
+        </TouchableOpacity>
         <TextInput
           style={styles.input}
           placeholder="Type a message..."
           value={message}
-          onChangeText={setMessage}
+          onChangeText={!freezeMessage ? setMessage : ""}
+          // onChangeText={setMessage}
           multiline
           onFocus={() => scrollViewRef.current.scrollToEnd({ animated: true })}
         />
@@ -157,6 +248,13 @@ const styles = StyleSheet.create({
   },
   sendButton: {
     padding: 10,
+  },
+  attachmentButton: {
+    padding: 10,
+  },
+  fileLink: {
+    color: "blue",
+    textDecorationLine: "underline",
   },
 });
 
